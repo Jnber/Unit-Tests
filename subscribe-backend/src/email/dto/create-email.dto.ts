9eaaa5b63db0e139862e829199d940d62679086f
Jnber <46068241+Jnber@users.noreply.github.com>
2022-12-15 10:54:56 +0100

    Delete subscribe-backend directory

--- subscribe-backend/src/email/dto/create-email.dto.ts
+++
@@ -1,9 +0,0 @@
-import { Column } from 'typeorm';
-import { IsEmail, IsNotEmpty } from 'class-validator';
-
-export class CreateEmailDto {
-  @Column()
-  @IsEmail()
-  @IsNotEmpty()
-  email: string;
-}
